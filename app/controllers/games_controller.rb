1f7572d7437bf209267e2ce0cafa1e053b99de2f
Lisa Li <lisa11@users.noreply.github.com>
2017-02-01 08:26:13 -0600

    updated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,14 +7,21 @@ class GamesController < ApplicationController
 
   def play_rock
     @player_move = "Rock"
+    @player_hand = "fa-hand-rock-o"
     @computer_move = ["Rock", "Paper", "Scissors"].sample
 
     if @computer_move == "Rock"
       @outcome = "tied"
+      @label_color = "label-warning"
+      @computer_hand = "fa-hand-rock-o"
     elsif @computer_move == "Paper"
       @outcome = "lose"
+      @label_color = "label-danger"
+      @computer_hand = "fa-hand-paper-o"
     else
       @outcome = "won"
+      @label_color = "label-success"
+      @computer_hand = "fa-hand-scissors-o"
     end
 
     render("games/play_game.html.erb")
@@ -22,14 +29,21 @@ class GamesController < ApplicationController
 
   def play_scissors
     @player_move = "Scissors"
+    @player_hand = "fa-hand-scissors-o"
     @computer_move = ["Rock", "Paper", "Scissors"].sample
 
     if @computer_move == "Rock"
       @outcome = "lose"
+      @label_color = "label-danger"
+      @computer_hand = "fa-hand-rock-o"
     elsif @computer_move == "Paper"
       @outcome = "won"
+      @label_color = "label-success"
+      @computer_hand = "fa-hand-paper-o"
     else
       @outcome = "tied"
+      @label_color = "label-warning"
+      @computer_hand = "fa-hand-scissors-o"
     end
 
     render("games/play_game.html.erb")
@@ -37,14 +51,21 @@ class GamesController < ApplicationController
 
   def play_paper
     @player_move = "Paper"
+    @player_hand = "fa-hand-paper-o"
     @computer_move = ["Rock", "Paper", "Scissors"].sample
 
     if @computer_move == "Rock"
       @outcome = "won"
+      @label_color = "label-success"
+      @computer_hand = "fa-hand-rock-o"
     elsif @computer_move == "Paper"
       @outcome = "tied"
+      @label_color = "label-warning"
+      @computer_hand = "fa-hand-paper-o"
     else
       @outcome = "lose"
+      @label_color = "label-danger"
+      @computer_hand = "fa-hand-scissors-o"
     end
 
     render("games/play_game.html.erb")
